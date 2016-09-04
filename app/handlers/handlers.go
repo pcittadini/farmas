767551d0ffe37230ba8d328cfcfae99c24be0cc5
pcittadini <p.cittadini@onehippo.com>
2016-09-04 21:05:04 +0200

    FARMAS-10 : consul deployed

--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -5,12 +5,17 @@ import (
 	"github.com/hashicorp/consul/api"
 	"net/http"
 	"log"
+	"os"
 )
 
+var consulHost = os.Getenv("COREOS_PRIVATE_IPV4")
 // Root endpoint
 func Root(c *gin.Context) {
-	res := `{"message":"root_handlers"}`
-	c.JSON(http.StatusOK, res)
+	res := `{"message":"root_handler","status":"healthy"}`
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(200)
+	c.Writer.Write([]byte(res))
 }
 
 // SetKey endpoint
@@ -20,11 +25,12 @@ func SetKey(c *gin.Context) {
 	value := c.Param("value")
 
 	log.Println("[ inserting ] key: " + key + ", value: " + value)
-	cfg := &api.Config{Address:"localhost:8500"}
+
+	addr := consulHost+":8500"
+	cfg := &api.Config{Address:addr}
 	csl, err := api.NewClient(cfg)
 	if err != nil {
-		c.JSON(http.StatusOK, err.Error())
-		c.Abort()
+		c.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
 
@@ -33,27 +39,40 @@ func SetKey(c *gin.Context) {
 	// PUT a new KV pair
 	p := &api.KVPair{Key: key, Value: []byte(value)}
 	_, err = kv.Put(p, nil)
+
 	if err != nil {
-		c.JSON(http.StatusOK, err.Error())
-		c.Abort()
-		return
-	}
-	// Lookup the pair
-	pair, _, err := kv.Get("foo", nil)
-	if err != nil {
-		c.JSON(http.StatusOK, err.Error())
-		c.Abort()
+		c.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	log.Printf("KV: %v", pair)
 	res := `{"message":"setkey_handler", "key":"added"}`
-
-	c.JSON(http.StatusOK, res)
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.WriteHeader(200)
+	c.Writer.Write([]byte(res))
 }
 
 // GetKey endpoint
 func GetKey(c *gin.Context){
-	res := `{"message":"getkey_handlers"}`
-    c.JSON(http.StatusOK, res)
+	//res := `{"message":"getkey_handlers"}`
+	key := c.Param("key")
+	addr := consulHost+":8500"
+	cfg := &api.Config{Address:addr}
+	csl, err := api.NewClient(cfg)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// Get a handle to the KV API
+	kv := csl.KV()
+
+	// Lookup the pair
+	pair, _, err := kv.Get(key, nil)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	log.Printf("[ get key ] KV: %v\n", pair)
+    c.JSON(http.StatusOK, pair)
 }
